56729fa44e608d1f77f70dcf25e0e53b5a15b0f5
Phil <pamramfam@gmail.com>
2023-09-19 14:53:22 -0400

    updated JS file

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () { 
+    const form = document.getElementById('.myForm'); 
+    const submitBtn = document.getElementById('.submit'); 
+
+    form.addEventListener('submit', function (e) { 
+        e.preventDefault(); 
+
+        window.location.href = 'formsubmitted.html'; 
+    })
+})
+
+
